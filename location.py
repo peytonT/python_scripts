676b932c44978a6c5a158ccb6a6ffbd0a2eeb682
peytonT <peyton.tran2013@gmail.com>
2016-05-30 22:17:42 -0400

    add location.py

---
+++ location.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+import webbrowser
+
+url = 'http://freegeoip.net/json/'
+host = sys.argv[1]      # 23.14.146.151
+res = requests.get('{}{}'.format(url, host))
+json_string = res.json()
+
+for key, val in json_string.items():
+    print('{}: {}'.format(key, val))
+
+google_maps_link = 'https://www.google.com/maps/place/{},{}'.format(json_string['latitude'], json_string['longitude'])
+
+# open google maps link of the location on a new tab on the default browser
+open_link = input('\nOpen google maps? (y/n): ')
+if open_link == 'y':
+    webbrowser.open_new_tab(google_maps_link)
+elif open_link == 'n':
+    pass
+else:
+    print('Invid input!')
